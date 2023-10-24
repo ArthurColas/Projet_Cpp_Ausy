8169f3cb8208a0fc1b0e7a6fd25b3a6e86cb4608
ArthurColas <arthur.colas@orange.fr>
2023-10-24 20:19:57 +0200

    main_serveur.cpp modifier, le serveur est encapsule dans une thread

--- main_server.cpp
+++ main_server.cpp
@@ -1,9 +1,13 @@
 #include "server.h"
 #include "server.cpp"
+#include <thread>
 
 int main() {
     int port = 12345;  // Change this to your desired port number
     TCPServer server(port);
-    server.StartListening();
+    
+    std::thread server_thread( [&server]() {server.StartListening();} );
+    server_thread.join();
+
     return 0;
 }
